a7806082d7e2f03c03fcde9eca7940bc834f4053
BalazsParducz <parducz.balazs@gmail.com>
2019-02-20 21:59:37 +0100

    created mapping in rest controller for all users get request

--- src/main/java/com/miniuser/restapi/controller/MiniController.java
+++ src/main/java/com/miniuser/restapi/controller/MiniController.java
@@ -1,7 +1,24 @@
 package com.miniuser.restapi.controller;
 
+import com.miniuser.restapi.model.MiniUser;
+import com.miniuser.restapi.repository.MiniUserRepo;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @RestController
 public class MiniController {
+
+    private MiniUserRepo userRepo;
+
+    public MiniController(MiniUserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping("/allUsers")
+    public Collection<MiniUser> getAllUsers() {
+        return userRepo.findAll().stream().collect(Collectors.toList());
+    }
 }
