650590d762af4cb63a224419b63c512c648773e1
BalazsParducz <parducz.balazs@gmail.com>
2019-02-20 17:52:37 +0100

    added subpackages and matching classes structured for web app

---
+++ src/main/java/com/miniuser/restapi/model/MiniUser.java
@@ -0,0 +1,7 @@
+package com.miniuser.restapi.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class MiniUser {
+}
---
+++ src/main/java/com/miniuser/restapi/repository/MiniUserRepo.java
@@ -0,0 +1,8 @@
+package com.miniuser.restapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MiniUserRepo extends JpaRepository {
+}
