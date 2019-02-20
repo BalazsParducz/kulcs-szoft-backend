d5280469cec2effcacaf4f8bd81157a063ab9354
BalazsParducz <parducz.balazs@gmail.com>
2019-02-20 21:19:58 +0100

    implemented database connectivity

--- src/main/java/com/miniuser/restapi/repository/MiniUserRepo.java
+++ src/main/java/com/miniuser/restapi/repository/MiniUserRepo.java
@@ -1,8 +1,9 @@
 package com.miniuser.restapi.repository;
 
+import com.miniuser.restapi.model.MiniUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MiniUserRepo extends JpaRepository {
+public interface MiniUserRepo extends JpaRepository<MiniUser, Integer> {
 }
