7f2131937068827b5707d7e68794bf7fd74bd867
BalazsParducz <parducz.balazs@gmail.com>
2019-02-20 16:58:51 +0100

    created basic project structure

---
+++ src/main/java/com/miniuser/restapi/MiniRestApplication.java
@@ -0,0 +1,13 @@
+package com.miniuser.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniRestApplication.class, args);
+    }
+
+}
