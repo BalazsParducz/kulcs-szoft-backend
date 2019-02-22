27484dcb4b2750ee5225deffd081dffe1913f232
BalazsParducz <parducz.balazs@gmail.com>
2019-02-22 09:26:57 +0100

    implemented web security

--- src/main/java/com/miniuser/restapi/config/SecurityConfig.java
+++ src/main/java/com/miniuser/restapi/config/SecurityConfig.java
@@ -1,7 +1,41 @@
 package com.miniuser.restapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${admin.password}")
+    private String password;
+
+    @Value("${admin.username}")
+    private String username;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(username)
+                .password(password)
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http)
+            throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
 }
